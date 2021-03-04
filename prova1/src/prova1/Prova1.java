515196b600a4a6032133e50765b464f1291def03
blondi.nicholas <blondi.nicholas@TAVE-PC06.OSDBSESTO.local>
2021-03-04 12:16:24 +0100

    init repo

---
+++ prova1/src/prova1/Prova1.java
@@ -0,0 +1,19 @@
+
+package prova1;
+
+/**
+ *
+ * @author blondi.nicholas
+ */
+public class Prova1 {
+
+
+    public static void main(String[] args) {
+
+        
+        
+        
+        
+    }
+    
+}
